b98167b5577e2e0796cb8934185b61cd6cd03c04
Filipe Petter <fgpetter@gmail.com>
2021-03-12 14:26:51 -0300

    Add CSS and icons libs

--- core/views/layouts/header.php
+++ core/views/layouts/header.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+  <title><?= $titulo ?></title>
 </head>
 <body>
