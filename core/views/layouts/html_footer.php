030b07cb10c457e800aaaecf67778da089c772a0
FILIPE PETTER <fgpetter@gmail.com>
2021-04-14 20:23:01 -0300

    Add Axios to project

--- core/views/layouts/html_footer.php
+++ core/views/layouts/html_footer.php
@@ -1,6 +1,7 @@
   <script src="assets/js/bootstrap.bundle.min.js"></script>
   <script src="assets/js/app.js"></script>
   <script src="assets/js/fontawesome.min.js"></script>
+  <script src="assets/js/axios.min.js"></script>
   <script>
     // Enable tooltip
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
